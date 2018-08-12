f2b772eb5b9e8b688192ddea28aa19eaa38482ab
zhgilevi <38855124+zhgilevi@users.noreply.github.com>
2018-08-12 08:35:40 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const TelegramBot = require('node-telegram-bot-api')
 
 
-const TOKEN='574004174:AAHLjKMltxU682v_7m6zQyj-pmNS5XRyE7g'
-
+const TOKEN='574004BBBEJRUEJSBBWNBWWH174:AAHLHDJEIEJDHHjKMltxU68DGDGSGHEHSHJSJDIIQHSGSGDVVSBB2v_7m6zQyj-pmNS5XRyE7g'
+Gbbhkjjjj
 const bot= new TelegramBot(TOKEN,{polling:true})
 
 
